dd67b0d8ba18c1abc0f9c10c537ee8fccc6d1b03
Éderson Cássio <edersoncassiolf@outlook.com>
2023-04-15 22:06:49 -0300

    Testando com sql.DB

---
+++ db/connection/connection.go
@@ -0,0 +1,36 @@
+package connection
+
+import (
+	"database/sql"
+	"fmt"
+
+	"com.derso/curso_creuto/gorm/util"
+)
+
+var (
+	// Conexão
+	Db *sql.DB
+)
+
+func ConnectDB() {
+	host := util.GetEnv("DEMO_DBHOST", "localhost")
+	dbPort := util.GetEnv("DEMO_DBPORT", "5432")
+	dbUser := util.GetEnv("DEMO_DBUSER", "postgres")
+	password := util.GetEnv("DEMO_DBPASSWORD", "mysecretpassword")
+	dbName := util.GetEnv("DEMO_DBNAME", "postgres")
+	sslMode := util.GetEnv("DEMO_DBSSL", "disable")
+	connectString := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		host, dbPort, dbUser, password, dbName, sslMode)
+
+	var err error
+	Db, err = sql.Open("postgres", connectString)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if err = Db.Ping(); err != nil {
+		panic(err)
+	}
+}
